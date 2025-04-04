68b772dc9c72b6f6fb64dff6fb734def83d8fec1
Thais <thaistsmartins@gmail.com>
2025-04-03 22:49:12 -0300

    Primeiro commit após recuperação de backup

--- App/urls.py
+++ App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import lista_usuarios, lista_produtos_disponiveis, detalhe_usuarios
 
 urlpatterns = [
-    path('usuarios/', lista_usuarios, name = 'lista_usuario'),
-    path('clientes/<int:pk>/', detalhe_usuarios, name = 'detalhe_usuario'),
+    path('usuarios/', lista_usuarios, name = 'lista_usuarios'),
+    path('clientes/<int:pk>/', detalhe_usuarios, name = 'detalhe_usuarios'),
     path('produtos', lista_produtos_disponiveis, name = 'lista_produtos_disponiveis'),
 ]
