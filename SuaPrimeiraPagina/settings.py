35863ee7316f36601574294fcdf635eb5844b828
Thais <thaistsmartins@gmail.com>
2025-04-15 23:14:26 -0300

    Configuração de pt-br, adequação de estilo e mensagens de erro

--- SuaPrimeiraPagina/settings.py
+++ SuaPrimeiraPagina/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -116,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
